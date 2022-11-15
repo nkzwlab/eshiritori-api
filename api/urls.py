10218de9913567eec6402f7844c58f7997907e8a
mokoshin0720 <shinya.work0720@gmail.com>
2022-11-16 05:16:45 +0900

    [fix]sort by asc

--- api/urls.py
+++ api/urls.py
@@ -8,5 +8,5 @@ router.register('testdata',views.TestDataViewSet)
 urlpatterns = [
     path('', include(router.urls)),
     path('img-with-start-char', views.HelloWorld.as_view(), name='test-get'),
-    path('histories', views.History.as_view(), name='history')
+    path('histories', views.History.as_view(), name='history') # TODO: IDでASCソートして返す
 ]
